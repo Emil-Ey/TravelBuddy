eebde9433f4cf01266521049941ba74d4690247a
Emil Kjærgaard Eybye <83577012+Emil-Ey@users.noreply.github.com>
2021-08-16 11:43:29 +0200

    First

---
+++ client/src/components/login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./../../logo.svg";
+import "./App.css";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import { Button, Paper } from "@material-ui/core";
+
+function Login() {
+	return (
+		<Container maxWidth="sm" className="App">
+			<Text>lol</Text>
+		</Container>
+	);
+}
+export default App;
