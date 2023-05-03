dca00994ca553400e2dee9bcb19094589b8bf824
Emil-Ey <Emil.Eybye@frameo.net>
2023-05-03 20:45:15 +0200

    Start testing pt. 2

---
+++ src/auth/auth.resolver.spec.ts
@@ -0,0 +1,52 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { v4 as uuidv4 } from 'uuid';
+import { UserLoginDto } from './auth.dto';
+
+describe('AuthResolver', () => {
+  let resolver: AuthResolver;
+  let uuid = uuidv4();
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthResolver,
+        {
+          provide: AuthService,
+          // using a factory just because
+          useFactory: () => ({
+            login: jest.fn((userLoginDto: UserLoginDto) => Promise.resolve({ accessToken: uuid })),
+            generateJwt: jest.fn((user: {_id: string, username: string, description: string}) => Promise.resolve({ accessToken: uuid })),
+          }),
+        },
+      ],
+    }).compile();
+
+    resolver = app.get<AuthResolver>(AuthResolver);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should login and get access token', async () => {
+      expect(
+        await resolver.login({username: "username", password: "password"})
+      ).toEqual(
+        { accessToken: uuid }
+      );
+    });
+  });
+
+  describe('logout', () => {
+    it('should logout and return true', async () => {
+      expect(
+        await resolver.logout()
+      ).toEqual(
+        true
+      );
+    });
+  });
+});
